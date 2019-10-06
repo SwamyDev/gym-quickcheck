9a72c566478f1dc50b76abb245503ac428981644
SwamyDev <b.raml@gmx.at>
2019-10-06 19:23:16 +0200

    Fix packaging error in tests package and setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(name='gym_quickcheck',
       version='0.0.0',
+      packages=find_packages(include='gym_quickcheck'),
       install_requires=['gym'],
       extras_require={'test': ['pytest']})
